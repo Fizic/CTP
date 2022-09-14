9c9b47d73c41c343a37ac8105bb2db2537aa796d
Firsov Kirill <kirya.firsov.04@gmail.com>
2022-09-14 19:41:41 +0300

    fix: create_schedule now the method, not function

--- main.py
+++ main.py
@@ -14,18 +14,17 @@ class Planet:
     def get_coords_from_time_y(self, time):
         return self.radius * sin(2 * pi * time / self.day_in_year)
 
+    def create_schedule(self, second_planet, time):
+        coords_x = []
+        coords_y = []
+        for i in range(11, time, 11):
+            coords_x.append(second_planet.get_coords_from_time_x(i) - self.get_coords_from_time_x(i))
+            coords_y.append(second_planet.get_coords_from_time_y(i) - self.get_coords_from_time_y(i))
+        plt.plot(coords_x, coords_y)
+        plt.show()
+
     def __str__(self):
-        return f'Planet {self.name} with radius {self.radius} and mass {self.mass} and day in year {self.day_in_year}'
-
-
-def create_schedule(first_planet, second_planet, time):
-    coords_x = []
-    coords_y = []
-    for i in range(11, time, 11):
-        coords_x.append(first_planet.get_coords_from_time_x(i) - second_planet.get_coords_from_time_x(i))
-        coords_y.append(first_planet.get_coords_from_time_y(i) - second_planet.get_coords_from_time_y(i))
-    plt.plot(coords_x, coords_y)
-    plt.show()
+        return f'Planet {self.name} with radius {self.radius} day in year {self.day_in_year}'
 
 
 mars = Planet('Mars', 1.52, 687)
@@ -34,7 +33,7 @@ saturn = Planet('Saturn', 9.58, 10759)
 venir = Planet('Venir', 0.72, 225)
 jupiter = Planet('Jupiter', 5.20, 4332)
 
-create_schedule(mars, earth, 12000)
-create_schedule(saturn, earth, 40000)
-create_schedule(venir, earth, 20000)
-create_schedule(earth, jupiter, 40000)
+earth.create_schedule(mars, 12000)
+earth.create_schedule(saturn, 23000)
+earth.create_schedule(venir, 40000)
+venir.create_schedule(jupiter, 32000)
